72431c136422eb6513820d2b7738237af483aac3
pavlo myroniuk <pspos.developqkation@gmail.com>
2019-11-15 13:11:27 +0200

    collect data from post /addbook request

--- routing/books.js
+++ routing/books.js
@@ -5,28 +5,38 @@ const Busboy = require('busboy');
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
+const dbwriter = require('../db/dbwriter');
 
 dotenv.config();
 
 const router = express.Router();
 
+const dbconfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+
 router.get('/addbook', (req, res) => {
-  console.log('add book GET');
   res.render('views/addbook', { layout: 'default', message: 'Have a book? Good idea to sell it' });
 });
 
 router.post('/addbook', (req, res) => {
+  const bookData = {};
   const busboy = new Busboy({ headers: req.headers });
   busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-    const saveTo = path.join(process.env.ROOT_DIR, 'uploads/' + filename);
-    file.pipe(fs.createWriteStream(saveTo));
+    const pathFile = path.join(process.env.ROOT_DIR, 'uploads/' + filename);
+    bookData['path'] = pathFile;
+    file.pipe(fs.createWriteStream(pathFile));
   });
-  busboy.on('field', (...args) => {
-    console.log('--------------------');
-    console.dir(args);
+  busboy.on('field', (name, value) => {
+    bookData[name] = value;
   });
   busboy.on('finish', () => {
     res.writeHead(200, { 'Connection': 'close' });
+    console.log(bookData);
     res.end("That's all folks!");
   });
   return req.pipe(busboy);
