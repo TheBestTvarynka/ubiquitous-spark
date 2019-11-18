d0c71f7bb8381f8926b308261334f46134914283
pavlo myroniuk <pspos.developqkation@gmail.com>
2019-11-19 00:32:55 +0200

    update routing/books.js (becouse I rewrite some parts in dbwriter)

--- routing/books.js
+++ routing/books.js
@@ -21,7 +21,7 @@ const dbconfig = {
 
 router.get('/addbook', (req, res) => {
   if (!req.session.name) {
-    req.cookie('redirect', '/addbook');
+    res.cookie('redirect', '/addbook');
     res.redirect('login');
   } else {
     console.log('get [book]: ', req.session.name);
@@ -30,26 +30,17 @@ router.get('/addbook', (req, res) => {
 });
 
 const addBook = (res, bookData) => {
-  const fieldsOrder = [];
-  const values = { value: [], function: [], array: [] };
-  const paths = bookData['path'];
-  const previews = bookData['preview'];
-  delete bookData.path;
-  delete bookData.preview;
-  for (const propereties in bookData) {
-    fieldsOrder.push(propereties);
-    values['value'].push(bookData[propereties]);
+  const types = [];
+  for (const field in bookData) {
+    types[field] = 'value';
   }
-  fieldsOrder.push('id');
-  values['function'].push(`nextval('book_id')`);
-  fieldsOrder.push('path');
-  values['array'].push(paths);
-  fieldsOrder.push('preview');
-  values['array'].push(previews);
+  types['path'] = 'array';
+  types['preview'] = 'array';
+  types['id'] = 'function';
+  bookData['id'] = `nextval('book_id')`;
   const pg = dbwriter.open(dbconfig);
   const writeData = pg.insert('books');
-  writeData.fields(fieldsOrder)
-           .value(values)
+  writeData.value(bookData, types)
            .then(result => {
              pg.close();
              console.log(result);
@@ -60,12 +51,13 @@ const addBook = (res, bookData) => {
 router.post('/addbook', (req, res) => {
   const bookData = { path: [], preview: [] };
   if (!req.session.name) {
-    req.cookies('redirect', '/addbook');
+    res.cookie('redirect', '/addbook');
     res.redirect('/login');
     return;
   }
   bookData['login'] = req.session.name;
   const busboy = new Busboy({ headers: req.headers });
+  // when we get a file
   busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
     const pathFile = path.join(process.env.ROOT_DIR, 'uploads/' + filename);
     if (fieldname === 'photos') {
@@ -76,9 +68,11 @@ router.post('/addbook', (req, res) => {
     }
     file.pipe(fs.createWriteStream(pathFile));
   });
+  // when we get just a field
   busboy.on('field', (name, value) => {
     bookData[name] = value;
   });
+  // when we finish
   busboy.on('finish', () => {
     // write all book data in db
     delete bookData.add;
