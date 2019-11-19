d4e28dc7c88e718ccac14b3b807c81a911efe487
vermi4elli <nikita.molodtsov21@gmail.com>
2019-11-19 03:06:28 +0200

    'Painted' a structure of a future 'book getter'

--- routing/search.js
+++ routing/search.js
@@ -2,4 +2,33 @@
 
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+const dbwriter = require('../db/dbwriter');
 
+const dbconfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+
+
+router.post('/search', (res, req) => {
+  //
+  // //here i'm 'parsing' results of the found books, but first I've got
+  // //to understand the principles of communicating with the db
+  //
+  // const pg = dbreader.open(dbconfig);
+  let list = [];
+  // pg.select('userdata')
+  //   .where({ login: user.login })
+  //   .then(rows => {
+  //     list = parseBooks(req, res, user, rows);
+  //   });
+
+  //here I'm creating nodes (books) and inserting them in the document
+
+});
