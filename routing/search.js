641afba54406ebd1bf3c145406eae35844e03f80
vermi4elli <nikita.molodtsov21@gmail.com>
2019-11-20 02:32:54 +0200

    Added some working (as I think) backend and modified the working (as I think) parsing of db

--- routing/search.js
+++ routing/search.js
@@ -1,45 +1,142 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const router = express.Router();
 // const fs = require('fs');
 // const path = require('path');
 // const dotenv = require('dotenv');
-// const dbreader = require('../db/dbreader');
-//
-// const dbconfig = {
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_DATABASE,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-// };
+const dbreader = require('../db/dbreader');
 
+const dbconfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+
+function parseBooks(rows) {
+  let arr = '';
+
+  rows.forEach(row => {
+    arr += '<div class=\'test\'><img src=\'' + row.preview + '\'><p>' +
+      row.author + ' - ' + row.name +
+      '</p><div class=\'price\'>100$</div></div>';
+  });
+
+  // again, just testing variant
+  //arr = '<div class=\'test\'><p>Your book</p><div class=\'price\'>100$</div></div>';
+  return arr;
+}
 
 router.post('/search', (req, res) => {
+  const filters = {
+    filter: req.body.select_filter,
+    value: req.body.value,
+    //valueEmpty === 1 - value is empty
+    valueEmpty: req.body.value === '',
+    publisher: req.body.publisher,
+    //publisherEmpty === 1 - publisher is empty
+    publisherEmpty: req.body.publisher === '',
+    year: req.body.year,
+    //yearEmpty === 1 - year is empty
+    yearEmpty: req.body.year === '',
+  };
+  console.log(filters);
 
-  //here i'm 'parsing' results of the found books, but first I've got
-  //to understand the principles of communicating with the db
+  const pg = dbreader.open(dbconfig);
+  let list = '';
 
-  console.log(req.body);
+  // testing the 'workability' of 'book' adding to the "results" div
+  // list = '<div class=\'test\'><p>Your book</p><div class=\'price\'>100$</div></div><div class=\'test\'><p>Your book</p><div class=\'price\'>100$</div></div><div class=\'test\'><p>Your book</p><div class=\'price\'>100$</div></div>';
+  // res.render('views/search', { layout: 'default', message: list });
+  // return;
 
-  // const filters = {
-  //   filterType: req.body.select_filter,
-  //   value: req.body.data
-  // };
-  // console.log(filters);
-
-  // const pg = dbreader.open(dbconfig);
-  //
-  // let list = [];
-  // pg.select('books')
-  //   .where({ name:  })
-  //   .then(rows => {
-  //     list = parseBooks(req, res, user, rows);
-  //   });
-  //
-  //here I'm creating nodes (books) and inserting them in the document
-
+  if (!filters.valueEmpty && filters.publisherEmpty && filters.yearEmpty) {
+    if (filters.filter === 'book') {
+      pg.select('books')
+        .where({ name: filters.value })
+        .then(rows => {
+          list = parseBooks(rows);
+        });
+    } else {
+      pg.select('books')
+        .where({ author: filters.value })
+        .then(rows => {
+          list = parseBooks(rows);
+        });
+    }
+  } else if (filters.valueEmpty && !filters.publisherEmpty && filters.yearEmpty) {
+    pg.select('books')
+      .where({ publishing: filters.publisher })
+      .then(rows => {
+        list = parseBooks(rows);
+      });
+  } else if (filters.valueEmpty && filters.publisherEmpty && !filters.yearEmpty) {
+    pg.select('books')
+      .where({ year: parseInt(filters.year) })
+      .then(rows => {
+        list = parseBooks(rows);
+      });
+  } else if (filters.valueEmpty && !filters.publisherEmpty && !filters.yearEmpty) {
+    pg.select('books')
+      .where({ year: parseInt(filters.year), publishing: filters.publisher })
+      .then(rows => {
+        list = parseBooks(rows);
+      });
+  } else if (!filters.valueEmpty && filters.publisherEmpty && !filters.yearEmpty) {
+    if (filters.filter === 'book') {
+      pg.select('books')
+        .where({ name: filters.value, year: parseInt(filters.year) })
+        .then(rows => {
+          list = parseBooks(rows);
+        });
+    } else {
+      pg.select('books')
+        .where({ author: filters.value, year: parseInt(filters.year) })
+        .then(rows => {
+          list = parseBooks(rows);
+        });
+    }
+  } else if (!filters.valueEmpty && !filters.publisherEmpty && filters.yearEmpty) {
+    if (filters.filter === 'book') {
+      pg.select('books')
+        .where({ name: filters.value, publishing: filters.publisher })
+        .then(rows => {
+          list = parseBooks(rows);
+        });
+    } else {
+      pg.select('books')
+        .where({ author: filters.value, publishing: filters.publisher })
+        .then(rows => {
+          list = parseBooks(rows);
+        });
+    }
+  } else if (!filters.valueEmpty && !filters.publisherEmpty && !filters.yearEmpty) {
+    if (filters.filter === 'book') {
+      pg.select('books')
+        .where({
+          name: filters.value,
+          publishing: filters.publisher,
+          year: parseInt(filters.year)
+        })
+        .then(rows => {
+          list = parseBooks(rows);
+        });
+    } else {
+      pg.select('books')
+        .where({
+          author: filters.value,
+          publishing: filters.publisher,
+          year: parseInt(filters.year)
+        })
+        .then(rows => {
+          list = parseBooks(rows);
+        });
+    }
+  }
+  console.log('List = ' + list);
+  res.render('views/search', { layout: 'default', message: list });
 });
 
 module.exports = router;
