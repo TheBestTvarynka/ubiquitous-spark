eeab04ee3978765e2a7d6b2ea9904b968a04b175
vermi4elli <nikita.molodtsov21@gmail.com>
2019-12-10 00:16:43 +0200

    Adding style for chat

--- routing/chat.js
+++ routing/chat.js
@@ -31,10 +31,18 @@ router.get('/chat', (req, res) => {
         const name = i.fullname;
         const letter = i.fullname.split('')[0];
         // eslint-disable-next-line max-len
-        resulting += '<a class="a"  href="chat_entry/' + name + '"><div class="admin"><div class="picture"><p class="letter">' + letter + '</p></div><p class="text"><strong>' + name + '</strong></p></div></a>';
+        resulting += '<a class="a"  href="/chat_entry/' + name + '"><div class="admin"><div class="picture"><p class="letter">' + letter + '</p></div><p class="text"><strong>' + name + '</strong></p></div></a>';
       });
       res.render('views/chat', { layout: 'default', admins: resulting });
     });
+});
+
+router.get('/chat_entry/:name', (req, res) => {
+  const fullname = req.params.name;
+  console.log('fullname accepted: ', fullname);
+  // eslint-disable-next-line no-unused-vars
+  const pg = dbreader.open(dbconfig);
+  res.render('views/chat_entry', { layout: 'default', admin: fullname });
 });
 
 module.exports = router;
